360068cd8163d71edde8c049ea4324d22c02eb10
juan <juan@kodefox.com>
2019-01-15 18:33:55 +0700

    change text to span

--- src/User.js
+++ src/User.js
@@ -1,12 +1,12 @@
 //@ flow
 import React from "react";
 
-export default class User extends React.Component {
+class User extends React.Component {
   state = {
     username: "Juan"
   };
   render() {
     let { username } = this.state;
-    return <text values={username}>${username}</text>;
+    return <span>${username}</span>;
   }
 }
