c32267504d7cea0502b9d22d48c087ac71f7aaa7
Filberts <filberts@filbert-kodefox.lan>
2019-01-15 16:58:34 +0700

    add home page

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component<{}> {
+  render() {
+    return (
+      <div className="App">
+            this is a home page.
+      </div>
+    );
+  }
+}
+
+export default App;
