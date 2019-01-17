6d199823c8f94691ca3c9c05b5f313034538fd8e
AlvinJK <alvin.junta@vospay.id>
2019-01-17 17:27:04 +0700

    Add Token Login

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,6 @@ import Home from './Home';
 import Contact from './Contact';
 import User from './User';
 import NotFound from './NotFound';
+import Login from './Login';
 
-export {About, Home, Contact, User, NotFound};
+export {About, Home, Contact, User, NotFound, Login};
