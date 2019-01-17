8d22aa96e0b750e41bd6e835afbe88d55327b2d2
greensirius <dominggus.octovianus@kodefox.com>
2019-01-17 02:45:22 -0600

    Adding navigation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
 // @flow
 
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
-class App extends Component<{}> {
+import {About, Home, Contact, NotFound, User} from './pages';
+import {BASEURL} from './constants/system';
+
+function Navigator() {
+  return (
+    <ul>
+      <li>
+        <NavLink to="/" activeStyle={{color: 'green'}}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={`${BASEURL}/about`}>About</NavLink>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/user/Filbert">Username 1</Link>
+      </li>
+      <li>
+        <Link to="/user/Alvin">Username 2</Link>
+      </li>
+    </ul>
+  );
+}
+
+type State = {
+  isLogin: boolean
+};
+class App extends React.Component<{}, State> {
+  state = {
+    isLogin: false
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <div>
+            <Navigator />
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path={`${BASEURL}/about`} component={About} />
+              <Route path="/contact/" component={Contact} />
+              <Route
+                path="/user/:username"
+                render={(props) => (
+                  <User username={props.match.params.username} />
+                )}
+              />
+              <Route component={NotFound} />
+            </Switch>
+            <input
+              type="button"
+              value={this.state.isLogin ? 'LOG-OUT' : 'LOG-IN'}
+              onClick={() => this.setState({isLogin: !this.state.isLogin})}
+            />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
